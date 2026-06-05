36d6e0630d2caf49239828a521e969608f5987c2
Juan Suazo <jpsuazo76@gmail.com>
2026-06-05 13:07:19 -0600

    inicio de repo

---
+++ src/lab7p1_juansuazo/Lab7P1_JuanSuazo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab7p1_juansuazo;
+
+/**
+ *
+ * @author juanp
+ */
+public class Lab7P1_JuanSuazo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
